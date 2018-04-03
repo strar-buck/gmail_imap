d719038069c29060d602a86fac0d609b1485abce
djangoconfire <djangopycon@gmail.com>
2018-04-03 14:37:11 +0530

    added constants

---
+++ constants.py
@@ -0,0 +1,11 @@
+# CREDENTIALS
+SCOPES = 'https://www.googleapis.com/auth/gmail.modify'
+CLIENT_SECRET_FILE = 'client_secret.json'
+APP_NAME = 'tenmiles'
+
+# AUTHENTICATION
+USERNAME = "ritupycon2@gmail.com"
+PASSWORD = ""
+
+# STORAGE
+EMAIL_DEST = "/storage"
